043f8c22d2ec8895f9888c6d1943140b4d2fe66b
Peter <peter@TCOM-Mac-mini.local>
2017-10-27 20:49:18 +0200

    Fixed git index issue

---
+++ app/packages/DependencyInjection/src/Exceptions/InvalidServiceCallbackTypeException.php
@@ -0,0 +1,12 @@
+<?php
+namespace Package\DependencyInjection\Exceptions;
+
+use Exception;
+
+class InvalidServiceCallbackTypeException extends Exception {
+
+	public function __construct($message='') {
+		parent::__construct($message);
+	}
+
+}
---
+++ app/packages/Translation/src/Config/Config.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+	########################
+	# Path to messages files
+	########################
+	'resources_path' => 'resources/',
+
+	###############################################################
+	# Throw error for missing message or missing message parameter.
+	###############################################################
+	'throw_missing_errors' => true
+);
