d55429742805a97d06d717493b22c351420d8866
PhoxPHP <taiwopeter35@gmail.com>
2018-01-07 20:46:01 +0200

    Composer update - phoxphp/glider update

--- public/config/database.php
+++ public/config/database.php
@@ -1,36 +1,33 @@
 <?php
 ##########################################
 # application database configuration file.
+# Note: PDO not available.
 ##########################################
 
 return [
-	#########################
-	# Database host
-	#########################
-	'db_host' => '',
-
-	#########################
-	# Database user
-	#########################
-	'db_user' => '',
-
-	#########################
-	# Database password
-	#########################
-	'db_password' => '',
-
-	#########################
-	# Database name
-	#########################
-	'db_name' => '',
-
-	########################
-	# Database adapter  
-	########################
-	'adapter' => 'pdo',
-
-	##############################
-	# Sqlite database storage path
-	##############################
-	'local_storage' => 'public/storage/database/'
+	'default' => [
+		'provider' => Kit\Glider\Platform\Mysqli\MysqliProvider::class,
+		'host' => 'localhost',
+		'alias' => 'mysqli',
+		'username' => 'root',
+		'password' => 'root',
+		'database' => 'test',
+		'charset' => 'utf8',
+		'collation' => '',
+		'domain' => 'example.com',
+		'auto_commit' => false,
+		'alt' => 'dev'
+	],
+	'dev' => [		'provider' => Kit\Glider\Platform\Pdo\PdoProvider::class,
+		'host' => 'localhost',
+		'alias' => 'pdo',
+		'username' => 'root',
+		'password' => 'root',
+		'database' => 'test',
+		'charset' => 'utf8',
+		'collation' => 'utf8',
+		'domain' => 'http://server.web/',
+		'auto_commit' => true,
+		'alt' => null
+	]
 ];
