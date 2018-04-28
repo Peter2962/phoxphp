007c0eae006e1b42e47f4f2f4495af8183b1d80e
Peter2962 <taiwop3@gmail.com>
2018-04-28 19:29:51 +0200

    Changed service variable to di in services.php and updated dependency injection

--- public/config/services.php
+++ public/config/services.php
@@ -1,21 +1,21 @@
 <?php
 #####################################################################################
 # Application services configuration file. All your services should be placed in here.
-# Do not change the variable `$service` as this is being used by the core app object.
+# Do not change the variable `$di` as this is being used by the core app object.
 #####################################################################################
 
-$service->register('cache', Kit\Cache\CacheManager::class);
+$di->register('cache', Kit\Cache\CacheManager::class);
 
-$service->register('config', App\Config::class);
+$di->register('config', App\Config::class);
 
-$service->register('auth', Kit\Auth\Auth::class);
+$di->register('auth', Kit\Auth\Auth::class);
 
-$service->register('request', Kit\Http\Request\RequestManager::class);
+$di->register('request', Kit\Http\Request\RequestManager::class);
 
-$service->register('session', Kit\Http\Session\Factory::class);
+$di->register('session', Kit\Http\Session\Factory::class);
 
-$service->register('response', Kit\Http\Response::class)
+$di->register('response', Kit\Http\Response::class)
 ->setDefaultParameters(new Kit\Http\Request\RequestManager());
 
-$service->register('en_msg', Kit\Translation\Factory::class)
+$di->register('en_msg', Kit\Translation\Factory::class)
 ->setDefaultParameters(new Kit\Translation\Locale\LocaleManager('en', 'us'));
