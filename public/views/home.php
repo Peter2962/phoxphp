44a351336be2a1986cd7a21ee2d2bbe677dbbec8
Peter2962 <taiwop3@gmail.com>
2018-05-26 19:13:17 +0200

    Updated home.php and layout.php

--- public/views/home.php
+++ public/views/home.php
@@ -2,7 +2,7 @@
 
 #viewAs<"content">
 	<div class="home">
-		<img src="/assets/img/logo-2018-05.png" alt="PhoxPHP logo">
+		<img src="assets/img/logo-2018-05.png" alt="PhoxPHP logo">
 		<h1>PhoxPHP Framework</h1>
 		<a href="https://www.phoxphp.com/docs/latest" target="_blank">View documentation</a>
 		<a href="https://www.phoxphp.com/docs/contributing" target="_blank">Contribute</a>
